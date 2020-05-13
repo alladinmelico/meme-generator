a55cb1552655c2cb10c8490e5653e7e9bb59964d
alladinMelico <melico.alladin@gmail.com>
2020-05-13 20:54:54 +0800

    initialized meme-generator

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            © 2020<a href="https://github.com/alladinmelico">Alladin Melico</a> 
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+ReactDOM.render(
+  <main>
+    <Header />
+    <App />
+    <Footer />
+  </main>,
+  document.getElementById('root')
+);
+serviceWorker.unregister();
